006cd5118944fa92a3b4d8a70a0a32f0cfe2150d
chocolatechipcookiejoe <chocolatechipcookiejoe@users.noreply.github.com>
2016-01-17 08:35:15 -0500

    Cleaned up code

--- src/watchface.c
+++ src/watchface.c
@@ -5,23 +5,25 @@ static TextLayer *bt_layer, *accel_layer;
  
 static void window_load(Window *window)
 {
-  // Init and set bt_layer text
+  // Init bt_layer
   bt_layer = text_layer_create(GRect(5, 5, 144, 30));
   text_layer_set_font(bt_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
   
-  // Init and set accel_layer text
-  accel_layer = text_layer_create(GRect(5, 45, 144, 30));
-  text_layer_set_font(accel_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-  text_layer_set_text(accel_layer, "No recent taps");
-  
+  // Set bt_layer text
   if (bluetooth_connection_service_peek() == true)
   {
     text_layer_set_text(bt_layer, "BT Connected");
   } else {
     text_layer_set_text(bt_layer, "BT Disconnected");
   }
+  
   // Add bt_layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(bt_layer));
+  
+  // Init and set accel_layer text
+  accel_layer = text_layer_create(GRect(5, 45, 144, 30));
+  text_layer_set_font(accel_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  text_layer_set_text(accel_layer, "No recent taps");
   
   // Add accel_layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(accel_layer));
@@ -30,6 +32,7 @@ static void window_load(Window *window)
 static void window_unload(Window *window)
 {
   text_layer_destroy(bt_layer);
+  text_layer_destroy(accel_layer);
 }
 
 static void bt_handler(bool connected)
